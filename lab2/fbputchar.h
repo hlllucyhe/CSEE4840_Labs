c0f7ece8195e76dd44fabfd1edbbe8b4e7000c37
Lucy He <lh3365@columbia.edu>
2026-02-19 15:57:29 -0500

    modify export .h

--- lab2/fbputchar.h
+++ lab2/fbputchar.h
@@ -1,11 +1,17 @@
 #ifndef _FBPUTCHAR_H
 #  define _FBPUTCHAR_H
 
+#include <linux/fb.h>
+
 #define FBOPEN_DEV -1          /* Couldn't open the device */
 #define FBOPEN_FSCREENINFO -2  /* Couldn't read the fixed info */
 #define FBOPEN_VSCREENINFO -3  /* Couldn't read the variable info */
 #define FBOPEN_MMAP -4         /* Couldn't mmap the framebuffer memory */
 #define FBOPEN_BPP -5          /* Unexpected bits-per-pixel */
+
+extern struct fb_var_screeninfo fb_vinfo;
+extern struct fb_fix_screeninfo fb_finfo;
+extern unsigned char *framebuffer;
 
 extern int fbopen(void);
 extern void fbputchar(char, int, int);
