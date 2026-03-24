aeb4efe7483c96aaef18144caf1b667aacb31780
wpp123789 <145554193+wpp123789@users.noreply.github.com>
2026-03-24 14:44:13 -0400

    Add files via upload

--- lab3/lab3-sw/vga_ball.h
+++ lab3/lab3-sw/vga_ball.h
@@ -3,19 +3,21 @@
 
 #include <linux/ioctl.h>
 
+/* Ball position struct */
 typedef struct {
-  unsigned char red, green, blue;
-} vga_ball_color_t;
-  
+    unsigned short x;  /* X coordinate (0-639) */
+    unsigned short y;  /* Y coordinate (0-479) */
+} vga_ball_position_t;
 
+/* Argument passed to ioctl */
 typedef struct {
-  vga_ball_color_t background;
+    vga_ball_position_t position;
 } vga_ball_arg_t;
 
 #define VGA_BALL_MAGIC 'q'
 
-/* ioctls and their arguments */
-#define VGA_BALL_WRITE_BACKGROUND _IOW(VGA_BALL_MAGIC, 1, vga_ball_arg_t)
-#define VGA_BALL_READ_BACKGROUND  _IOR(VGA_BALL_MAGIC, 2, vga_ball_arg_t)
+/* ioctl commands */
+#define VGA_BALL_WRITE_POSITION _IOW(VGA_BALL_MAGIC, 1, vga_ball_arg_t)
+#define VGA_BALL_READ_POSITION  _IOR(VGA_BALL_MAGIC, 2, vga_ball_arg_t)
 
 #endif
